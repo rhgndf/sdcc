aafef3b607cd2f1dcb036a675875a39f0f04563c
Jie Feng <hjf3108@gmail.com>
2026-07-10 17:07:12 +0800

    Bound 78K0 regressions to target resources

--- support/regression/ports/78k0/spec.mk
+++ support/regression/ports/78k0/spec.mk
@@ -19,7 +19,7 @@ ifdef CROSSCOMPILING
   SDCCFLAGS += -I$(top_srcdir)
 endif
 
-SDCCFLAGS += -m78k0 --less-pedantic --out-fmt-ihx --stack-loc 0xf000
+SDCCFLAGS += -m78k0 -DSTACK_SIZE=256 --less-pedantic --out-fmt-ihx --stack-loc 0xf000
 LINKFLAGS += 78k0.lib
 
 OBJEXT = .rel

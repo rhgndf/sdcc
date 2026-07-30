825e0a276f97ae48cf4f9c02ebf11be522d7af69
Maarten Brock <sourceforge.brock@dse.nl>
2026-07-30 22:16:20 +0000

    * support/regression/tests/bug-3393.c: fix stm8 endianness
    git-svn-id: https://svn.code.sf.net/p/sdcc/code/trunk/sdcc@16740 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- support/regression/tests/bug-3393.c
+++ support/regression/tests/bug-3393.c
@@ -30,11 +30,16 @@ int f (void)
       if (v.a != 1) return 1;
       if (v.i != 2) return 2;               /* holds 0: overwritten by init for d */
       if (v.j != 3) return 3;               /* holds 0: overwritten by init for d */
-#if defined(__SDCC) /* only check for SDCC as it assumes little-endianness */
-#if 0 /* still fails because i,j & c are flattened into S */
+#if defined(__SDCC) /* only check for SDCC as it assumes integer size and endianness */
+# if 0 /* still fails because i,j & c are flattened into S */
       if (v.c != 0x32) return 4;            /* holds 0: overwritten by init for d */
-#endif
+# endif
+
+# if defined(__SDCC_stm8)
+      if ((v.d >> 8) != 0x32) return 5;     /* holds 0: none left */
+# else
       if ((v.d & 0xFF) != 0x32) return 5;   /* holds 0: none left */
+# endif
 #endif
       if (v.z != 4) return 6;               /* holds 0: none left */
       return 0;
@@ -46,11 +51,16 @@ int g (void)
       if (w.a != 5) return 1;
       if (w.i != 6) return 2;               /* holds 0: overwritten by init for d */
       if (w.j != 7) return 3;               /* holds 0: overwritten by init for d */
-#if defined(__SDCC) /* only check for SDCC as it assumes little-endianness */
-#if 0 /* still fails because i,j & c are flattened into S */
+#if defined(__SDCC) /* only check for SDCC as it assumes integer size and endianness */
+# if 0 /* still fails because i,j & c are flattened into S */
       if (w.c != 0x76) return 4;            /* holds 0: overwritten by init for d */
-#endif
+# endif
+
+# if defined(__SDCC_stm8)
+      if ((w.d >> 8) != 0x76) return 5;     /* holds 0: none left */
+# else
       if ((w.d & 0xFF) != 0x76) return 5;   /* holds 0: none left */
+# endif
 #endif
       if (w.z != 8) return 6;               /* holds 0: none left */
       return 0;
